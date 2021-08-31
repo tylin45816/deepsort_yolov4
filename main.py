ed553af491e7be1285a1775934aac6e92a0d375f
teddy <ty.lin45816@gmail.com>
2021-08-31 14:50:57 +0800

    bbx_cls_done

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 from math import inf
-from typing import final
+from numpy.core.fromnumeric import shape
 import torch
 import cv2
 import os
@@ -60,7 +60,30 @@ class VideoTracker(object):
     def __exit__(self, exc_type, exc_value, exc_traceback):
         if exc_type:
             print(exc_type, exc_value, exc_traceback)
+    
+    def bbox_transform(self,boxes,im):
+        output = []
+        wh = np.flip(im.shape[0:2])
+        for class_index in range(len(boxes)-1):
+            cxcy = tuple((np.array(boxes[class_index][0:2]) * wh).astype(np.int64))
+            bbx_wh = tuple((np.array(boxes[class_index][2:4]) * wh).astype(np.int64))
+            cx, cy = cxcy
+            w, h = bbx_wh
+            x1 = int(abs(cx-w/2))
+            y1 = int(abs(cy-h/2)*0.97)
+            x2 = int(cx+w/2)
+            y2 = int((cy+h/2)*1.03)
+            classes = int(boxes[class_index][6])
+            if x1 < 0:
+                continue
             
+            im = cv2.circle(im,((cx),(cy)),2,(0,0,255),cv2.FILLED)
+            im = cv2.circle(im,((x1),(y1)),3,(255,0,255),cv2.FILLED)
+            im = cv2.circle(im,((x2),(y2)),3,(0,255,255),cv2.FILLED)
+            
+            output.append([x1,y1,x2,y2,classes])
+        return output
+    
     def run(self):
         idx_frame = 0
         downward_counter = [0,0,0,0,0]
@@ -75,68 +98,45 @@ class VideoTracker(object):
             start = time.time()
             _, ori_im = self.vdo.retrieve()
             im = cv2.cvtColor(ori_im, cv2.COLOR_BGR2RGB)
-            # print(im)
             
-            #depent on data
+            """depend on data"""
             up_linepos = 317
             line = [(190, 317), (900, 317)]
-            # im = cv2.line(im,(190,up_linepos),(900,up_linepos),(255,0,0),3,cv2.LINE_AA)
+            ori_im = cv2.line(ori_im,(190,up_linepos),(900,up_linepos),(255,0,0),3,cv2.LINE_AA)
             # up_linepos = 500
             # line = [(160, up_linepos), (1600, up_linepos)]
             # result_img2 = cv2.line(img,(160,up_linepos),(1600,up_linepos),(255,0,0),3,cv2.LINE_AA)
 
-            # do detection
-            # im=Image.fromarray(im)
+            """do detection"""
             height, width = im.shape[:2]
-            # sized = im.resize((self.m.width, self.m.height))
             sized=cv2.resize(im,(self.detector.width,self.detector.height))
-            print(type(sized))
-
             boxes = do_detect(self.detector, sized, 0.5, 0.4, 1)
-            boxes = torch.tensor(boxes)
+            
+            bbx_cls = self.bbox_transform(boxes,ori_im)
+            bbx_cls = np.array(bbx_cls)
+            print("bbx_cls\n",bbx_cls)
+
             if len(boxes) == 0:
                 print("===No detection===")
                 continue
-            print("boxes\n",boxes)
+            boxes = torch.tensor(boxes)
             bbox = boxes[:, :4]
-            print(boxes)
-            bbox_xywh = bbox * torch.FloatTensor([[width, height, width, height]])
-            print("torch.tensor\n",torch.FloatTensor([[width, height, width, height]]))
+            bbox_cxcywh = bbox * torch.FloatTensor([[width, height, width, height]])
             cls_conf = boxes[:, 5]
-            classes = boxes[:,6]
-            print("classes",classes)
 
-            # bbox_xywh, cls_conf, cls_ids = self.detector(im)
-
-            if bbox_xywh is not None:
-                # select person class
-                # mask = cls_ids==0
-
-                # bbox_xywh = bbox_xywh[mask]
-                bbox_xywh[:, 3:] *= 1.2  # bbox dilation just in case bbox too small
-                # cls_conf = cls_conf[mask]
-
-                # do tracking
-                outputs = self.deepsort.update(bbox_xywh, cls_conf, im)
-                # draw boxes for visualization
-                im_crops = []
-                offset = 0
-                if len(outputs) > 0:
-                    print("current_detection\n",outputs)
-                    for inform in outputs:
-                        x1, y1, x2, y2 = inform[:4]
-                        final_crops = ori_im[y1+offset:y2+offset, x1+offset:x2+offset]
-                        print("11111",(final_crops.shape))
-                        final_crops = cv2.resize(final_crops,(self.detector.width,self.detector.height))
-                        print("22222",(final_crops.shape))
-                        if len(final_crops) != 0:
-                            final_box = do_detect(self.detector,final_crops,0.7,0.4,1)
-                            print("==final_detect",final_box)
-                    
-                    # bbox_xyxy = outputs[:, :4]
-                    # print("bbox_xyxy\n",bbox_xyxy)
-                    # identities = outputs[:, -1]
-                    # ori_im = draw_boxes(ori_im, bbox_xyxy, identities)
+            if bbox_cxcywh is not None:
+                bbox_cxcywh[:, 3:] *= 1.2  # bbox dilation just in case bbox too small
+                
+                """do tracking"""
+                bbx_ids = self.deepsort.update(bbox_cxcywh, cls_conf, im)
+                
+                """draw boxes for visualization"""
+                if len(bbx_ids) > 0:
+                    print("bbx_ids\n",bbx_ids)
+                    print("============")
+                    bbox_xyxy = bbx_ids[:, :4]
+                    identities = bbx_ids[:, -1]
+                    ori_im = draw_boxes(ori_im, bbox_xyxy, identities)
                 
             np.random.seed(42)
             COLORS = np.random.randint(0, 255, size=(200, 3),dtype="uint8")
@@ -146,7 +146,7 @@ class VideoTracker(object):
             # indexIDs = []
             # memory = []
             # bbx = []
-            # #extract data from sort
+            """extract data from sort"""
             # for track in outputs:
             #     current_box.append([track[0], track[1],track[2], track[3],track[5]])
             #     indexIDs.append(int(track[4]))
@@ -163,7 +163,7 @@ class VideoTracker(object):
             
             # # result_img2 = draw.draw_boxes(img,bbx,indexIDs)
             
-            # #data processing  class and  counting 
+            """data processing  class and  counting """
             # if len(current_box) > 0:
             #     i = int(0)
             #     for box in current_box:
@@ -207,22 +207,33 @@ class VideoTracker(object):
             print("time: {:.03f}s, fps: {:.03f}".format(end - start, 1 / (end - start)))
             print("===========End of a frame===========")
 
+            """左邊文字背景框"""
+            # cv2.rectangle(result_img2, (0, 0), (160, 80), (85, 0, 0), -1)
+            # cv2.putText(result_img2, "Car : " + str(downward_counter[1]), (0, 30), cv2.FONT_HERSHEY_COMPLEX, .6, (170, 255, 50),1)
+            # cv2.putText(result_img2, "motorcycle : " + str(downward_counter[2]), (0, 50), cv2.FONT_HERSHEY_COMPLEX, .6, (50, 255, 50),1)
+            # cv2.putText(result_img2, "truck : " + str(downward_counter[4]), (0, 70), cv2.FONT_HERSHEY_COMPLEX, .6, (255, 250, 150),1)
+            """右邊文字背景框"""
+            # cv2.rectangle(result_img2, (740, 0), (900, 80), (85, 0, 0), -1)
+            # cv2.putText(result_img2, "Car : " + str(upward_counter[1]), (740, 30), cv2.FONT_HERSHEY_SIMPLEX, .6, (170, 255, 50),1)
+            # cv2.putText(result_img2, "motorcycle : " + str(upward_counter[2]), (740, 50), cv2.FONT_HERSHEY_SIMPLEX, .6, (170, 255, 50),1)
+            # cv2.putText(result_img2, "truck : " + str(upward_counter[4]), (740, 70), cv2.FONT_HERSHEY_SIMPLEX, .6, (170, 255, 50),1)
+        
             if self.args.display:
                 cv2.imshow("test", ori_im)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
+                if cv2.waitKey(0) & 0xFF == ord('q'):
                     break
-
-            if self.args.save_path:
-                self.writer.write(ori_im)
+        
+        self.vdo.release()
+        cv2.destroyAllWindows()
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("VIDEO_PATH", type=str)
-    parser.add_argument("--config_detection", type=str, default="./configs/yolov4.yaml")
+    parser.add_argument("--cfg_detection", type=str, default="./configs/yolov4.yaml")
     # yolov4.cfg/yolov4.weights/yolo-obj.cfg/yolo-obj_best.weights
-    parser.add_argument("--config_deepsort", type=str, default="./configs/deep_sort.yaml")
+    parser.add_argument("--cfg_deepsort", type=str, default="./configs/deep_sort.yaml")
     parser.add_argument("--ignore_display", dest="display", action="store_false", default=True)
-    parser.add_argument("--frame_interval", type=int, default=1)
+    parser.add_argument("--frame_interval", type=int, default=2)
     parser.add_argument("--display_width", type=int, default=800)
     parser.add_argument("--display_height", type=int, default=600)
     parser.add_argument("--save_path", type=str, default="./demo/demo.avi")
@@ -232,7 +243,7 @@ def parse_args():
 if __name__ == '__main__':
     args=parse_args()
     cfg=get_config()
-    cfg.merge_from_file(args.config_detection)
-    cfg.merge_from_file(args.config_deepsort)
+    cfg.merge_from_file(args.cfg_detection)
+    cfg.merge_from_file(args.cfg_deepsort)
     with VideoTracker(cfg, args) as vdo_trk:
         vdo_trk.run()
